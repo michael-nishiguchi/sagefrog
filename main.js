74f11714165758a50e3b1b5a3c9f8d065c68805a
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2021-07-15 17:34:25 -0400

    cleanup css

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-document.getElementById("openLightbox").addEventListener("click", openLightbox);
-document.getElementById("lightbox").addEventListener("click", openLightbox);
-document.getElementById("menu").addEventListener("click", toggleMenu);
-document.getElementById("close").addEventListener("click", toggleMenu);
+document.getElementById('openLightbox').addEventListener('click', openLightbox);
+document.getElementById('lightbox').addEventListener('click', openLightbox);
+document.getElementById('menu').addEventListener('click', toggleMenu);
+document.getElementById('close').addEventListener('click', toggleMenu);
 
 function openLightbox() {
-  document.getElementById("lightbox").classList.toggle("showLightbox");
+	document.getElementById('lightbox').classList.toggle('showLightbox');
 }
 
-
 function toggleMenu() {
-  document.getElementById("nav-text").classList.toggle("show-nav");
-  document.getElementById("mobile-delivery").classList.toggle("show-delivery");
-  document.getElementById("menu").classList.toggle("hide-menu");
-  document.getElementById("close").classList.toggle("show-close");
+	document.getElementById('nav-text').classList.toggle('show-nav');
+	document.getElementById('mobile-delivery').classList.toggle('show-delivery');
+	document.getElementById('menu').classList.toggle('hide-menu');
+	document.getElementById('close').classList.toggle('show-close');
 }
